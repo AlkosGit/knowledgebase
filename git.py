f148811859b02c6eade6bca75f83fd9276bb247c
AlkosGit <jsalkema@gmx.com>
2018-12-05 18:11:20 +0100

    Add git doc

---
+++ git.py
@@ -0,0 +1,17 @@
+# file: git.py
+from listprops import ListProps
+
+class Doc(ListProps):
+    """
+    Create new repository:
+    Log in to github.com and create a new repository.
+        Initialize local repository:
+            $ git init
+        Add files to index:
+            $ git add .
+        Commit changes:
+            $ git commit -m "<description>"
+        Connect to online repository:
+            $ git remote add origin https://github.com/AlkosGit/<reponame>.git
+            $ git push -u origin master
+    """
